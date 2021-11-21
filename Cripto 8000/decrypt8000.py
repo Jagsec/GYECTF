3ba1aa2ab5c18e3b2b653ed3ceb032614d44fa27
Jagsec <62567044+Jagsec@users.noreply.github.com>
2021-11-21 11:53:55 -0500

    second commit

--- Cripto 8000/decrypt8000.py
+++ Cripto 8000/decrypt8000.py
@@ -1,13 +1,15 @@
 cipher = u'籐籢籎籌籝籏粄籬簼粃簽类籨簹籿簼类籨簹粁籁簹簹簹籨籱籾籱籪籱籪籱籪粆'
-decimal_values = []
-offset = 0x1F40
-x = 23800
-while x > 23000:
+offset = 0x8000
+x = 0x0
+while x < offset:
     flag = ''
     for char in cipher:
-        value = int(hex(ord(char)),16) - offset - x
-        flag += chr(value)
+        value = int(hex(ord(char)),16) - x
+        try:
+            flag += chr(value)
+        except ValueError:
+            pass
     if flag.startswith('G'):
         print(x)
         print(flag)
-    x -= 1
+    x += 1
